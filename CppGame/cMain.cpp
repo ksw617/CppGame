8d6ff781c061fc80948503c4e11fc080584eeaf0
ksw617 <ksw617@gmail.com>
2024-02-19 19:26:10 +0900

    Update cMain.cpp

--- CppGame/cMain.cpp
+++ CppGame/cMain.cpp
@@ -27,15 +27,8 @@ enum COLOR
 
 #pragma region Struct
 
-struct Player
-{
-	bool act;
-	int x;
-	int y;
-	const char* shape;
-};
 
-struct Enemy
+struct Obj
 {
 	bool act;
 	int x;
@@ -48,9 +41,8 @@ struct Enemy
 
 
 #pragma region Game
-Player* player = nullptr;
-Enemy* enemy = nullptr;
-
+Obj* player = nullptr;
+Obj* enemy = nullptr;
 
 void StageInitialize();
 void StageProgress();
@@ -76,7 +68,7 @@ int main()
 		system("cls");
 		StageProgress();
 		StageRender();
-		Sleep(20);	  
+		Sleep(50);	  
 
 	}
 
@@ -89,18 +81,80 @@ int main()
 void StageInitialize()
 {
 	
+	player = (Obj*)malloc(sizeof(Obj));
+	player->act = true;
+	player->x = 5;
+	player->y = 5;
+	player->color = WHITE;
+	player->shape[0] = "¡á¡á¡á¡á";
+	player->shape[1] = "¡á¡á¡á¡á";
+	player->shape[2] = "¡á¡á¡á¡á";
+
+	enemy = (Obj*)malloc(sizeof(Obj));
+	enemy->act = true;
+	enemy->x = 10;
+	enemy->y = 10;
+	enemy->color = YELLOW;
+	enemy->shape[0] = "¡á¡á¡á¡á";
+	enemy->shape[1] = "¡á¡á¡á¡á";
+	enemy->shape[2] = "¡á¡á¡á¡á";
 
 }
 
 void StageProgress()
 {
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		player->x--;
+	}
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		player->x++;
 
+	}
+	if (GetAsyncKeyState(VK_UP))
+	{
+		player->y--;
+
+	}
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		player->y++;
+	}
+
+	//A : Enemy, B : Player
+
+	if (player->x < enemy->x + 4 && enemy->x < player->x + 4 &&
+		player->y < enemy->y + 3 && enemy->y < player->y + 3)
+	{
+		enemy->color = RED;
+	}
+	else
+	{
+		enemy->color = YELLOW;
+	}
 }
 
 
 void StageRender()
 {
-	
+
+	for (int i = 0; i < 3; i++)
+	{
+		MovePos(enemy->x, enemy->y + i);
+		Paint(enemy->color);
+		printf(enemy->shape[i]);
+
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		MovePos(player->x, player->y + i);
+		Paint(player->color);
+		printf(player->shape[i]);
+
+	}
+
 }
 
 void StageRelease()
