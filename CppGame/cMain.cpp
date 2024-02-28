ba40cc5cb781f57d6966961327b15c3b3dac353f
ksw617 <ksw617@gmail.com>
2024-02-28 19:42:00 +0900

    update

--- CppGame/cMain.cpp
+++ CppGame/cMain.cpp
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <time.h>
 
+
+
+#pragma region DoubleBuffer
+
+#define BufferWidth 80	  //가로 버퍼 크기
+#define BufferHeight 40	  //세로 버퍼 크기
+
+HANDLE hBuffer[2];	// 창 두개를 제어하는 핸들
+int screeIndex;		//hBuffer[screeIndex], screenIndex == 0 or 1
+void InitBuffer();
+void FlipBuffer();
+void ClearBuffer();
+void WriteBuffer(int x, int y, const char* shape, int color);
+void ReleaseBuffer();
+#pragma endregion
 #pragma region Enum
 enum COLOR
 {
@@ -30,23 +46,84 @@ enum SCENE_ID
 	 STAGE,
 };
 
+enum DIR
+{
+	IDLE,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+};
 
-#pragma region DoubleBuffer
+#pragma endregion
+#pragma region Map
 
-#define BufferWidth 80	  //가로 버퍼 크기
-#define BufferHeight 40	  //세로 버퍼 크기
+#define MAP_WIDTH 40
+#define MAP_HEIGHT 40
+int map[MAP_HEIGHT][MAP_WIDTH] =
+{
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+};
 
-HANDLE hBuffer[2];	// 창 두개를 제어하는 핸들
-int screeIndex;		//hBuffer[screeIndex], screenIndex == 0 or 1
-void InitBuffer();
-void FlipBuffer();
-void ClearBuffer();
-void WriteBuffer(int x, int y, const char* shape, int color);
-void ReleaseBuffer();
 #pragma endregion
 
-#pragma region PlayGame
+#pragma region Struct
+struct Obj
+{
+	int step;
+	int maxMove;
+	DIR dir;
+	int x;
+	int y;
+	COLOR color;
+	const char* shape[3];
 
+};
+
+#pragma endregion
+
+
+
+#pragma region PlayGame
 SCENE_ID id;
 
 void LogoInitialize();
@@ -59,6 +136,9 @@ void MenuProgress();
 void MenuRender();
 void MenuRelease();
 
+Obj* player = nullptr;
+Obj* enemy = nullptr;
+
 void StageInitialize();
 void StageProgress();
 void StageRender();
@@ -70,8 +150,9 @@ void PlayGame();
 
 int main()
 {
-	id = LOGO;
-	LogoInitialize();
+	srand((unsigned)time(NULL));
+	id = STAGE;
+	StageInitialize();
 	InitBuffer();
 
 	while (true)
@@ -81,7 +162,7 @@ int main()
 		FlipBuffer();
 		ClearBuffer();
 
-		Sleep(30);
+		Sleep(50);
 	}
 
 	ReleaseBuffer();
@@ -154,19 +235,256 @@ void MenuRelease()
 }
 #pragma endregion
 
+bool CheckMap(Obj* obj)
+{
+	for (int y = 0; y < 3; y++)
+	{
+		for (int x = 0; x < 3; x++)
+		{
+			if (map[obj->y + y][obj->x + x] == 1)
+			{
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
 #pragma region Stage
+
+int enemyCount = 0;
 void StageInitialize()
 {
+	player = (Obj*)malloc(sizeof(Obj));
+	player->x = 10;
+	player->y = 11;
+	player->color = WHITE;
+	player->shape[0] = "■■■";
+	player->shape[1] = "■■■";
+	player->shape[2] = "■■■";
+
+	enemy = (Obj*)malloc(sizeof(Obj));
+
+
+	do
+	{
+		enemy->x = rand() % 40;
+		enemy->y = rand() % 40;
+		enemyCount++;
+
+	} while (CheckMap(enemy));
+
+	enemy->dir = (DIR)(rand() % 5);
+	enemy->step = 0;
+	enemy->maxMove = rand() % 5 + 5;
+
+	enemy->color = RED;
+	enemy->shape[0] = "■■■";
+	enemy->shape[1] = "■■■";
+	enemy->shape[2] = "■■■";
 }
+
+bool IsCollisionMap(int x, int y, DIR dir)
+{
+	switch (dir)
+	{
+	case LEFT:
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[y + i][x] == 1)
+			{
+				return true;
+			}
+		}
+		break;
+	case RIGHT:
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[y + i][x + 2] == 1)
+			{
+				return true;
+			}
+		}
+		break;
+	case UP:
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[y][x + i] == 1)
+			{
+				return true;
+			}
+		}
+		break;
+	case DOWN:
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[y + 2][x + i] == 1)
+			{
+				return true;
+			}
+		}
+		break;
+	default:
+		break;
+	}
+
+	return false;
+}
+
 void StageProgress()
 {
+	enemy->step++;
+	if (enemy->step > enemy->maxMove)
+	{
+		enemy->step = 0;
+		enemy->maxMove = rand() % 5 + 5;
+		enemy->dir = (DIR)(rand() % 5);
+	}
+	
+	switch (enemy->dir)
+	{
+	case LEFT:
+		enemy->x--;
+		if (IsCollisionMap(enemy->x, enemy->y, LEFT))
+		{
+			enemy->x++;
+			enemy->dir = (DIR)(rand() % 5);
+			enemy->step = 0;
+		}
+		break;
+	case RIGHT:
+		enemy->x++;
+		if (IsCollisionMap(enemy->x, enemy->y, RIGHT))
+		{
+			enemy->x--;
+			enemy->dir = (DIR)(rand() % 5);
+			enemy->step = 0;
+		}
+		break;
+	case UP:
+		enemy->y--;
+		if (IsCollisionMap(enemy->x, enemy->y, UP))
+		{
+			enemy->y++;
+			enemy->dir = (DIR)(rand() % 5);
+			enemy->step = 0;
+		}
+		break;
+	case DOWN:
+		enemy->y++;
+		if (IsCollisionMap(enemy->x, enemy->y, DOWN))
+		{
+			enemy->y--;
+			enemy->dir = (DIR)(rand() % 5);
+			enemy->step = 0;
+		}
+		break;
+	default:
+		break;
+	}
+
+	if (GetAsyncKeyState(VK_LEFT))
+	{
+		if (map[player->y + 0][player->x - 1] == 0 && map[player->y + 1][player->x - 1] == 0&& map[player->y + 2][player->x - 1] == 0)
+		{
+			player->x--;
+		}
+	}
+	if (GetAsyncKeyState(VK_RIGHT))
+	{
+		int enemyCount = 0;
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[player->y + i][player->x + 3] == 0)
+			{
+				enemyCount++;
+			}
+		}
+
+		if (enemyCount == 3)
+		{
+			player->x++;
+		}
+		
+	}
+	if (GetAsyncKeyState(VK_UP))
+	{
+		player->y--;
+		for (int i = 0; i < 3; i++)
+		{
+			if (map[player->y][player->x + i] == 1)
+			{
+				player->y++;
+				break;
+			}
+		}
+
+	}
+	if (GetAsyncKeyState(VK_DOWN))
+	{
+		player->y++;
+		if (IsCollisionMap(player->x, player->y, DOWN))
+		{
+			player->y--;
+		}
+	}
+
+	if (player->x <= enemy->x + 2 && enemy->x <= player->x + 2 &&
+		player->y <= enemy->y + 2 && enemy->y <= player->y + 2)
+	{
+		exit(true);
+	}
 }
 void StageRender()
 {
-	WriteBuffer(10, 10, "STAGE", BLUE);
+	
+
+	for (int y = 0; y < MAP_HEIGHT; y++)
+	{
+		for (int x = 0; x < MAP_WIDTH; x++)
+		{
+			switch (map[y][x])
+			{
+			case 1:
+				WriteBuffer(x,y,"■", YELLOW);
+				break;
+			default:
+				break;
+			}
+
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		WriteBuffer(player->x, player->y + i, player->shape[i], player->color);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		WriteBuffer(enemy->x, enemy->y + i, enemy->shape[i], enemy->color);
+	}
+
+	char sNum[5];
+
+	_itoa_s(enemyCount, sNum, 10);
+
+	WriteBuffer(38, 0, sNum, RED);
+
 }
 void StageRelease()
 {
+	if (player != nullptr)
+	{
+		free(player);
+		player = nullptr;
+	}
+
+	if (enemy != nullptr)
+	{
+		free(enemy);
+		enemy = nullptr;
+	}
 }
 #pragma endregion
 
